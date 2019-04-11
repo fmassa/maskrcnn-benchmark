8803fdc45cc6c6a97de079a2b0de21a126ba0680
Francisco Massa <fvsmassa@gmail.com>
2019-04-11 08:26:52 -0700

    Simplify FPN

--- maskrcnn_benchmark/modeling/backbone/fpn.py
+++ maskrcnn_benchmark/modeling/backbone/fpn.py
@@ -24,20 +24,15 @@ class FPN(nn.Module):
                 FPN output, and the result will extend the result list
         """
         super(FPN, self).__init__()
-        self.inner_blocks = []
-        self.layer_blocks = []
-        for idx, in_channels in enumerate(in_channels_list, 1):
-            inner_block = "fpn_inner{}".format(idx)
-            layer_block = "fpn_layer{}".format(idx)
-
+        self.inner_blocks = nn.ModuleList()
+        self.layer_blocks = nn.ModuleList()
+        for in_channels in in_channels_list:
             if in_channels == 0:
                 continue
             inner_block_module = conv_block(in_channels, out_channels, 1)
             layer_block_module = conv_block(out_channels, out_channels, 3, 1)
-            self.add_module(inner_block, inner_block_module)
-            self.add_module(layer_block, layer_block_module)
-            self.inner_blocks.append(inner_block)
-            self.layer_blocks.append(layer_block)
+            self.inner_blocks.append(inner_block_module)
+            self.layer_blocks.append(layer_block_module)
         self.top_blocks = top_blocks
 
     def forward(self, x):
@@ -48,21 +43,18 @@ class FPN(nn.Module):
             results (tuple[Tensor]): feature maps after FPN layers.
                 They are ordered from highest resolution first.
         """
-        last_inner = getattr(self, self.inner_blocks[-1])(x[-1])
+        last_inner = self.inner_blocks[-1](x[-1])
         results = []
-        results.append(getattr(self, self.layer_blocks[-1])(last_inner))
+        results.append(self.layer_blocks[-1](last_inner))
         for feature, inner_block, layer_block in zip(
             x[:-1][::-1], self.inner_blocks[:-1][::-1], self.layer_blocks[:-1][::-1]
         ):
             if not inner_block:
                 continue
             inner_top_down = F.interpolate(last_inner, scale_factor=2, mode="nearest")
-            inner_lateral = getattr(self, inner_block)(feature)
-            # TODO use size instead of scale to make it robust to different sizes
-            # inner_top_down = F.upsample(last_inner, size=inner_lateral.shape[-2:],
-            # mode='bilinear', align_corners=False)
+            inner_lateral = inner_block(feature)
             last_inner = inner_lateral + inner_top_down
-            results.insert(0, getattr(self, layer_block)(last_inner))
+            results.insert(0, layer_block(last_inner))
 
         if isinstance(self.top_blocks, LastLevelP6P7):
             last_results = self.top_blocks(x[-1], results[-1])
