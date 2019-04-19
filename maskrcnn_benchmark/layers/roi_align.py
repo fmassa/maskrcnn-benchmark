0695f9b42d4c9193556257fed2499127f8c7cbd1
Francisco Massa <fvsmassa@gmail.com>
2019-04-19 01:53:39 -0700

    Remove unecessary files

--- maskrcnn_benchmark/layers/roi_align.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from torchvision.ops import roi_align
-from torchvision.ops import RoIAlign as ROIAlign
--- maskrcnn_benchmark/layers/roi_pool.py
+++
@@ -1,3 +0,0 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from torchvision.ops import roi_pool
-from torchvision.ops import RoIPool as ROIPool
